f2be14a9f0dba75174e3245e128c6219bed9302d
Munavvar Popatiya <tatva.dotnet.munavvarpopatiya@outlook.com>
2024-06-14 18:22:17 +0530

    csda

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgApexchartsModule } from "ng-apexcharts";
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    
+  ]
+})
+export class AdminModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AddExpensesComponent } from './expenses/addExpenses/addExpenses.component';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {path:"",component:AddExpensesComponent}
+    {path:"",component:DashboardComponent}
 ];
